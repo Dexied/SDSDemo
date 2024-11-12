e124300949c1967a35a4bb4559e5c75b790b013e
Dexied <33525453+Dexied@users.noreply.github.com>
2024-11-13 04:31:03 +0500

    550-000

--- PluginBuild.bat
+++ PluginBuild.bat
@@ -1,1 +1,1 @@
-B:\UnrealEngine\UE_5.4\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="B:\Projects\SDSDemo\Plugins\SimpleDialogueSystem\SimpleDialogueSystem.uplugin" -Package="B:\Projects\SDSDemo\Builds\SimpleDialogueSystem" -Rocket -2019
+B:\UnrealEngine\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="B:\Projects\SDSDemo\Plugins\SimpleDialogueSystem\SimpleDialogueSystem.uplugin" -Package="B:\Projects\SDSDemo\Builds\SimpleDialogueSystem" -Rocket -2019
