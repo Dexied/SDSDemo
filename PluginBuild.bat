1764571e01f098f27b922babb1c70b8ac16c9c68
Dexied <33525453+Dexied@users.noreply.github.com>
2025-11-19 20:42:56 +0500

    570-000

--- PluginBuild.bat
+++ PluginBuild.bat
@@ -1,1 +1,4 @@
-B:\UnrealEngine\UE_5.6\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="B:\Projects\SDSDemo\Plugins\SimpleDialogueSystem\SimpleDialogueSystem.uplugin" -Package="B:\Projects\SDSDemo\Builds\SimpleDialogueSystem" -Rocket -2019
+@echo off
+B:\UnrealEngineSource\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="B:\Projects\SDSDemo\Plugins\SimpleDialogueSystem\SimpleDialogueSystem.uplugin" -Package="B:\Projects\SDSDemo\Builds\SimpleDialogueSystem" -Rocket -2019 > "B:\Projects\SDSDemo\BuildLog.txt" 2>&1
+echo Build completed. Check BuildLog.txt for details.
+pause
